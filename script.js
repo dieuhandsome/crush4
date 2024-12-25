cb90c98457747722d0b8589e40ad896b6a20758a
dieuhandsome <93961636+dieuhandsome@users.noreply.github.com>
2024-12-25 23:34:45 +0900

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ noButton.addEventListener("click", function () {
 });
 
 function handleYesClick() {
-  titleElement.innerHTML = "Anh iu bé ,Anh hứa hongg làm bé buồn nữa đouuu :3";
+  titleElement.innerHTML = "Anh iu em ,Anh hứa hongg làm bé buồn nữa đouuu :3";
   buttonsContainer.classList.add("hidden");
   changeImage("yes");
 }
@@ -42,12 +42,11 @@ function resizeYesButton() {
 
 function generateMessage(noCount) {
   const messages = [
-    "Không Bao Giờ",
-    "Anh bicc lỗi rồi ạa",
-    "Mong bé tha lỗi choo anhh :((",
+    
+    "Anh bicc lỗi rồi a",
+    "chơi với anhh đi:((",
     "Anhh saii rồi , anhh đáng trách ạ",
-    "Bé đừng giận anhh nữa nhoo",
-    "Anhhh iu bé nhắm nhunnn đóoooo",
+    
   ];
 
   const messageIndex = Math.min(noCount, messages.length - 1);
